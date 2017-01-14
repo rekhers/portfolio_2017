d4bdb45aa401b4ac8cc52b7380c17ef9bb721368
Rekha Tenjarla <rekha.tenjarla@gmail.com>
2017-01-14 04:21:53 -0500

    gradients, animation arrows, sidebar dot navigation

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,44 +1,99 @@
 var destination = document.getElementById("container");
 
-class Allo extends React.Component{
-  	render(){
-  		return <div> {this.props.children} </div>;
-  	}
+
+/*
+*
+* Shared Elements
+*
+*/
+
+class DotNavigation extends React.Component{
+  render(){
+    return <svg id="dots" height="140" width="50">
+        <circle className="selected" cx="20" cy="10" r="7" stroke="white" fill="white"/>
+       <circle cx="20" cy="40" r="6" fill="none" stroke="black" />
+        <circle cx="20" cy="70" r="6" fill="none" stroke="black"/>
+        <circle cx="20" cy="100" r="6" fill="none" stroke="black"/>
+        <circle cx="20" cy="130" r="6" fill="none" stroke="black"/>
+      </svg>
   }
+}
 
+class DownArrow extends React.Component{
+  render(){
+    return <div className="arrow-down"></div>;
+  } 
 
-class Greeting extends React.Component{
-	  render(){
-	    return <h1>Hello, {this.props.name}</h1>;
-	  }
 }
 
 
-class headerText extends React.Component{
-	render(){
-		return 
-	}
-}; 
 
-class Mountain extends React.Component{
-    render() {
-        return <div><img src={'./public/bw-resized.jpg'} className="subway"/></div>;
-    }
-};
 
+
+/*
+*
+* SECTIONS
+*
+*/
 
 class HeaderText extends React.Component{
-	render(){
-		return <div><div className="name"><div id="firstName">rekha</div><div id="lastName">tenjarla</div> </div> <div className="links"> <div id="abt">ABOUT ME</div> <div id="projects">PROJECTS</div><div id="contact">CONTACT</div></div></div>;
-	}
+  render(){
+    return <div className="name"> REKHA TENJARLA </div>;
+  }
 
 };
+
+class About extends React.Component{
+  render(){
+    return <div className="name"> pageTwo </div>;
+  }
+
+};
+
+
+var pageOne = (<div>
+               <HeaderText/>
+               <DotNavigation/>
+               <DownArrow />
+               </div>
+  );
+
+var pageTwo = (<div>
+               <About/>
+               <DotNavigation/>
+                <DownArrow />
+               </div>
+  )
+
 
 
     ReactDOM.render(
-    	<div>
-    	<Mountain/>  
-    	    	<HeaderText/>    	
-  	
-    	 </div>
+    	pageOne
 ,    	destination);
+
+
+
+$("#container").on("click", function(){
+
+    ReactDOM.render(
+        pageTwo,
+        destination
+      );
+
+$("body").css("background", "-webkit-linear-gradient(130deg, #db4a74 10%, #ac4adc 90%)"); 
+
+ // $( "body" ).animate({
+ //     background: "-webkit-linear-gradient(30deg, #6441A5 10%, #2a0845 90%)"
+
+ //  }, 5000, function() {
+ //    // Animation complete.
+ //  });
+
+});
+
+
+
+
+
+
+
