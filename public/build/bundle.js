3ed7b831e763d59a16f911b077604cf8b1d3a102
Rekha Tenjarla <rekha.tenjarla@gmail.com>
2017-09-25 16:17:33 -0400

    responsive component tagging in projects/tools

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -20697,7 +20697,7 @@ var ProjectDiv = function (_React$Component) {
 		//list of projects and associated skills, with inherited props list of skills that reflect user's selection
 		var _this = _possibleConstructorReturn(this, (ProjectDiv.__proto__ || Object.getPrototypeOf(ProjectDiv)).call(this, props));
 
-		_this.state = { skills: _this.props.skills, projects: { toptracks: { skills: ["javascript", "react", "webpack", "html", "css"], src: "public/assets/toptracks.png", description: " Lists a user's top scrobbled tracks over different periods of time, built with React and the Last.fm API. In the process of adding authentication.", style: { width: "14em" } }, tweets: { skills: ["javascript", "html", "css", "socketio"], src: "public/assets/tweets.png", description: "An ongoing project that uses the Twitter API, Google Maps API, and the Natural Language Toolkit to visualize, filter, and analyze linguistic trends. This app was born out of a final project for a college computational linguistics course, in which I analyzed twitter corpora to compare whether the new england vernacular 'wicked' was being used as an adjective or adverb in any given context. 'That party was wicked' (adj) vs. 'She's wicked smart' (adv).", style: { width: "14em" } }, d3lifeglobe: { skills: ["javascript", "d3", "html", "css", "node"], src: "public/assets/d3lifeglobe.png", description: "An animated d3 globe that rotates to a given coordinate and projects a path between the places that I've lived.", style: { width: "15em" } }, previousprofile: { skills: ["javascript", "d3", "grunt", "less", "css", "html", "node"], src: "public/assets/previousprofile.png", description: "My previous profile -- I'm proud of some of the d3 data visualizations that I built for it and not so proud of the lack of responsiveness.", style: { width: "12em" } }, thisprofile: { skills: ["react", "webpack", "css", "less", "html", "node"], src: "", description: "This single page app was built with react which allowed me to flexibly integrate this filter function class component with other pure component sections. Building this app tested my commitment to inline styles with React. Since many of these components require animation that I'm accustomed to using pseudo selectors and css for, I opted to ", style: { width: "50rem", color: "#FFF", height: "10rem", padding: "3rem", backgroundColor: "#333333", border: "1px solid #F25F5C" }, text: "this profile :)", display: "flex", justifyContent: "center", flexDirection: "row", alignItems: "center" } } };
+		_this.state = { skills: _this.props.skills, projects: { toptracks: { skills: ["javascript", "react", "webpack", "html", "css"], src: "public/assets/toptracks.png", description: " Lists a user's top scrobbled tracks over different periods of time, built with React and the Last.fm API. In the process of adding authentication.", style: { width: "14em" } }, tweets: { skills: ["javascript", "html", "css", "socketio"], src: "public/assets/tweets.png", description: "An ongoing project that uses the Twitter API, Google Maps API, and the Natural Language Toolkit to visualize, filter, and analyze linguistic trends. This app was born out of a final project for a college computational linguistics course, in which I analyzed twitter corpora to compare whether the new england vernacular 'wicked' was being used as an adjective or adverb in any given context. 'That party was wicked' (adj) vs. 'She's wicked smart' (adv).", style: { width: "14em" } }, d3lifeglobe: { skills: ["javascript", "d3", "html", "css", "node"], src: "public/assets/d3lifeglobe.png", description: "An animated d3 globe that rotates to a given coordinate and projects a path between the places that I've lived.", style: { width: "15em" } }, previousprofile: { skills: ["javascript", "d3", "grunt", "less", "css", "html", "node"], src: "public/assets/previousprofile.png", description: "My previous profile -- I'm proud of some of the d3 data visualizations that I built for it and not so proud of the lack of responsiveness.", style: { width: "12em" } }, thisprofile: { skills: ["react", "webpack", "css", "less", "html", "node"], src: "", description: "This single page app was built with react which allowed me to flexibly integrate this filter function class component with other pure component sections. Building this app tested my commitment to inline styles with React. Since many of these components require animation that I'm accustomed to using pseudo selectors and css for, I opted to ", style: { width: "10rem", color: "#FFF", height: "10rem", padding: "3rem", backgroundColor: "#333333", border: "1px solid #F25F5C" }, text: "this profile :)", display: "flex", justifyContent: "center", flexDirection: "row", alignItems: "center" } } };
 
 		return _this;
 	}
@@ -20746,6 +20746,11 @@ var ProjectDiv = function (_React$Component) {
 
 				var skills = _underscore2.default.values(this.state.skills);
 				var projects = this.state.filteredProjects;
+
+				console.log(skills);
+
+				console.log(projects);
+
 				if (projects.length > 0) {
 
 					return _react2.default.createElement(
@@ -20760,8 +20765,25 @@ var ProjectDiv = function (_React$Component) {
 									_react2.default.createElement(
 										'div',
 										{ className: 'description' },
-										' ',
-										projects[key].description
+										_react2.default.createElement(
+											'div',
+											{ className: 'descText' },
+											' ',
+											projects[key].description,
+											' '
+										),
+										_react2.default.createElement(
+											'div',
+											{ className: 'skillTags' },
+											Object.keys(_underscore2.default.intersection(projects[key].skills, skills)).map(function (k, i) {
+												return _react2.default.createElement(
+													'div',
+													{ className: 'tag', key: i },
+													_underscore2.default.intersection(projects[key].skills, skills)[k],
+													' '
+												);
+											})
+										)
 									)
 								);
 							} else {
@@ -20778,8 +20800,25 @@ var ProjectDiv = function (_React$Component) {
 									_react2.default.createElement(
 										'div',
 										{ className: 'description' },
-										' ',
-										projects[key].description
+										_react2.default.createElement(
+											'div',
+											{ className: 'descText' },
+											' ',
+											projects[key].description,
+											' '
+										),
+										_react2.default.createElement(
+											'div',
+											{ className: 'skillTags' },
+											Object.keys(_underscore2.default.intersection(projects[key].skills, skills)).map(function (k, i) {
+												return _react2.default.createElement(
+													'div',
+													{ className: 'tag', key: i },
+													_underscore2.default.intersection(projects[key].skills, skills)[k],
+													' '
+												);
+											})
+										)
 									)
 								);
 							}
@@ -20861,65 +20900,6 @@ var Projects = exports.Projects = function (_React$Component2) {
 
 	return Projects;
 }(_react2.default.Component);
-
-// <div className="javascript react webpack" className="picHolderMedium"> 
-// 					<img className="mediumpic" src='public/assets/toptracks.png'/>
-// 					<div className="overlay">  
-// 						<div className="text">TOP TRACKS <br/> <br/> Lists a user's top scrobbled tracks over different periods of time, built with
-// 						 React and the Last.fm API. In the process of adding authentication. </div>
-// 					 </div> 
-// 				</div> 
-
-
-// 				<div "javascript socketio node"  className="picHolder">  
-// 					<img className="pic"src="public/assets/tweets.png" />
-// 			<div className="overlay"> 
-
-// 					<div className="text">WICKED TWITTA <br/> <br/>
-// 						An ongoing project that uses the Twitter API, Google Maps API, and the Natural Language Toolkit to visualize, filter, and analyze linguistic trends. 
-// 						This app was born out of a final project for a college computational linguistics course, in which I analyzed twitter corpora to compare whether the new england vernacular "wicked" 
-// 						was being used as an adjective or adverb in any given context. "That party was wicked" (adj) vs. "She's wicked smart" (adv).  <a href="http://www.github.com/rekhers"> github </a>
-// 					</div>
-
-// 				</div>
-
-// 				</div> 
-
-
-// 				<div className="picHolderMedium">  
-// 					<img className="mediumpic" src="public/assets/tweets-usmap.png" />
-// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-// 						 React and the Last.fm API </div>
-// 				</div> 
-
-
-// 				<div className="picHolder"> 
-// 					<img className="pic" src='public/assets/d3-life-globe.png'/>
-// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-// 						 React and the Last.fm API </div>
-// 				</div> 
-
-
-// 				<div className="picHolderThin">  
-// 					<img className="thinpic" src='public/assets/previousprofile.png'/>
-// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-// 						 React and the Last.fm API </div>
-// 				</div> 
-
-// 				<div className="picHolderThin">  
-// 					<img className="thinpic" src='public/assets/txchoropleth.png'/>
-// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-// 						 React and the Last.fm API </div>
-
-// 				</div> 
-
-
-// 				<div className="picHolderMedium">  
-// 					<img className="mediumpic" src='public/assets/wsumaptooltip.png'/>
-// 					<div className="overlay"> <div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-// 						 React and the Last.fm API </div></div> 
-
-// 				</div>
 
 /***/ }),
 /* 96 */
--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -12,7 +12,7 @@ class ProjectDiv extends React.Component{
 		super(props);
 
 		//list of projects and associated skills, with inherited props list of skills that reflect user's selection
-		this.state = {skills: this.props.skills, projects: {toptracks: {skills: ["javascript", "react", "webpack", "html", "css"], src: "public/assets/toptracks.png", description: " Lists a user's top scrobbled tracks over different periods of time, built with React and the Last.fm API. In the process of adding authentication.", style: {width: "14em"}}, tweets:{skills:["javascript", "html", "css", "socketio"], src: "public/assets/tweets.png", description: "An ongoing project that uses the Twitter API, Google Maps API, and the Natural Language Toolkit to visualize, filter, and analyze linguistic trends. This app was born out of a final project for a college computational linguistics course, in which I analyzed twitter corpora to compare whether the new england vernacular 'wicked' was being used as an adjective or adverb in any given context. 'That party was wicked' (adj) vs. 'She's wicked smart' (adv).",  style: {width: "14em"}}, d3lifeglobe: {skills: ["javascript", "d3", "html", "css", "node"], src: "public/assets/d3lifeglobe.png",  description: "An animated d3 globe that rotates to a given coordinate and projects a path between the places that I've lived.",  style: {width: "15em"}}, previousprofile: {skills: ["javascript", "d3", "grunt", "less", "css", "html", "node"], src: "public/assets/previousprofile.png",  description: "My previous profile -- I'm proud of some of the d3 data visualizations that I built for it and not so proud of the lack of responsiveness.", style: {width: "12em"}}, thisprofile: {skills: ["react","webpack", "css", "less", "html", "node"], src: "",  description: "This single page app was built with react which allowed me to flexibly integrate this filter function class component with other pure component sections. Building this app tested my commitment to inline styles with React. Since many of these components require animation that I'm accustomed to using pseudo selectors and css for, I opted to ", style: {width: "50rem", color:"#FFF", height: "10rem", padding: "3rem", backgroundColor: "#333333", border: "1px solid #F25F5C"}, text:"this profile :)", display: "flex", justifyContent: "center", flexDirection: "row", alignItems: "center"}}}
+		this.state = {skills: this.props.skills, projects: {toptracks: {skills: ["javascript", "react", "webpack", "html", "css"], src: "public/assets/toptracks.png", description: " Lists a user's top scrobbled tracks over different periods of time, built with React and the Last.fm API. In the process of adding authentication.", style: {width: "14em"}}, tweets:{skills:["javascript", "html", "css", "socketio"], src: "public/assets/tweets.png", description: "An ongoing project that uses the Twitter API, Google Maps API, and the Natural Language Toolkit to visualize, filter, and analyze linguistic trends. This app was born out of a final project for a college computational linguistics course, in which I analyzed twitter corpora to compare whether the new england vernacular 'wicked' was being used as an adjective or adverb in any given context. 'That party was wicked' (adj) vs. 'She's wicked smart' (adv).",  style: {width: "14em"}}, d3lifeglobe: {skills: ["javascript", "d3", "html", "css", "node"], src: "public/assets/d3lifeglobe.png",  description: "An animated d3 globe that rotates to a given coordinate and projects a path between the places that I've lived.",  style: {width: "15em"}}, previousprofile: {skills: ["javascript", "d3", "grunt", "less", "css", "html", "node"], src: "public/assets/previousprofile.png",  description: "My previous profile -- I'm proud of some of the d3 data visualizations that I built for it and not so proud of the lack of responsiveness.", style: {width: "12em"}}, thisprofile: {skills: ["react","webpack", "css", "less", "html", "node"], src: "",  description: "This single page app was built with react which allowed me to flexibly integrate this filter function class component with other pure component sections. Building this app tested my commitment to inline styles with React. Since many of these components require animation that I'm accustomed to using pseudo selectors and css for, I opted to ", style: {width: "10rem", color:"#FFF", height: "10rem", padding: "3rem", backgroundColor: "#333333", border: "1px solid #F25F5C"}, text:"this profile :)", display: "flex", justifyContent: "center", flexDirection: "row", alignItems: "center"}}}
 
 	}
 
@@ -56,6 +56,11 @@ class ProjectDiv extends React.Component{
 
 		const skills = _.values(this.state.skills);
 		const projects = this.state.filteredProjects;
+
+		console.log(skills);
+
+		console.log(projects);
+
 		if(projects.length > 0) {
  
 			return (<div id="project-container" className="project-container">
@@ -63,12 +68,30 @@ class ProjectDiv extends React.Component{
 					if(projects[key].src){
 	  					return (<div key={i} className="picHolder">
 	  							<img className="pic" src={projects[key].src} style={projects[key].style}/>
-	  							<div className="description"> {projects[key].description}</div>
+	  							<div className="description"> 
+	  								<div className="descText"> {projects[key].description} </div>
+	  									<div className="skillTags"> 
+	  									{Object.keys(_.intersection(projects[key].skills, skills)).map(function(k, i){
+	  										return  <div className="tag" key={i}> 
+	  											{_.intersection(projects[key].skills, skills)[k]} </div> 
+	  									}
+	  									)}
+	  									</div> 
+	  							</div>
 								</div>)
 	  				} else { 
 	  					 return (<div key={i} className="picHolder">
 	  							<div style={projects[key].style}> {projects[key].text} </div> 
-	  							<div className="description"> {projects[key].description}</div>
+	  						<div className="description"> 
+	  								<div className="descText"> {projects[key].description} </div>
+	  									<div className="skillTags"> 
+	  									{Object.keys(_.intersection(projects[key].skills, skills)).map(function(k, i){
+	  										return  <div className="tag" key={i}> 
+	  											{_.intersection(projects[key].skills, skills)[k]} </div> 
+	  									}
+	  									)}
+	  									</div> 
+	  							</div>
 								</div>)
 	  				}
 					}
@@ -127,64 +150,4 @@ export class Projects extends React.Component{
 	}
 }
 
-
-
-	// <div className="javascript react webpack" className="picHolderMedium"> 
-	// 					<img className="mediumpic" src='public/assets/toptracks.png'/>
-	// 					<div className="overlay">  
-	// 						<div className="text">TOP TRACKS <br/> <br/> Lists a user's top scrobbled tracks over different periods of time, built with
-	// 						 React and the Last.fm API. In the process of adding authentication. </div>
-	// 					 </div> 
-	// 				</div> 
-
-					
-	// 				<div "javascript socketio node"  className="picHolder">  
-	// 					<img className="pic"src="public/assets/tweets.png" />
-	// 			<div className="overlay"> 
-
-	// 					<div className="text">WICKED TWITTA <br/> <br/>
-	// 						An ongoing project that uses the Twitter API, Google Maps API, and the Natural Language Toolkit to visualize, filter, and analyze linguistic trends. 
-	// 						This app was born out of a final project for a college computational linguistics course, in which I analyzed twitter corpora to compare whether the new england vernacular "wicked" 
-	// 						was being used as an adjective or adverb in any given context. "That party was wicked" (adj) vs. "She's wicked smart" (adv).  <a href="http://www.github.com/rekhers"> github </a>
-	// 					</div>
-
-	// 				</div>
-
-	// 				</div> 
-
-					
-	// 				<div className="picHolderMedium">  
-	// 					<img className="mediumpic" src="public/assets/tweets-usmap.png" />
-	// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-	// 						 React and the Last.fm API </div>
-	// 				</div> 
-
-
-	// 				<div className="picHolder"> 
-	// 					<img className="pic" src='public/assets/d3-life-globe.png'/>
-	// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-	// 						 React and the Last.fm API </div>
-	// 				</div> 
-
-
-	// 				<div className="picHolderThin">  
-	// 					<img className="thinpic" src='public/assets/previousprofile.png'/>
-	// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-	// 						 React and the Last.fm API </div>
-	// 				</div> 
-
-	// 				<div className="picHolderThin">  
-	// 					<img className="thinpic" src='public/assets/txchoropleth.png'/>
-	// 					<div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-	// 						 React and the Last.fm API </div>
-
-	// 				</div> 
-
-
-	// 				<div className="picHolderMedium">  
-	// 					<img className="mediumpic" src='public/assets/wsumaptooltip.png'/>
-	// 					<div className="overlay"> <div className="text">Top Tracks -- Lists a user's top scrobbled tracks over different periods of time, built with
-	// 						 React and the Last.fm API </div></div> 
-
-	// 				</div> 
 
